fb67fb745e467540fb4812e7c6e79c50f516cfdb
owlSuri <98316357+owlsuri@users.noreply.github.com>
2022-02-25 09:44:05 +0900

    꿀팀 1번째

---
+++ AboutPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import main from './assets/main.png';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView} from 'react-native';
+
+export default function AboutPage() {
+  console.disableYellowBox = true;
+  return ()
+
+}
+
+const styles = StyleSheet.create({})
